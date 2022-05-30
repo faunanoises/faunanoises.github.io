8e0dc9ea67d7e10365df85060a79e3e48419cd5f
faunanoises <105766614+faunanoises@users.noreply.github.com>
2022-05-30 16:43:37 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ringer from "./flipdish-ringer.mp3"; //array?
+import "./styles.css";
+
+const Sound = () => {
+  const audio = new Audio(ringer);
+  return (
+    <div>
+      <button
+        onClick={() => {
+		  audio = audio[Math.round(Math.random() * (audio.length - 1))]
+          audio.play();
+        }}
+      >
+        Play
+      </button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Sound />, rootElement);
